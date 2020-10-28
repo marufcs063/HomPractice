fa448433b3651c913c7c0344edb147e5155b7d70
Abdullah Basar Maruf <marufcs063@gmail.com>
2020-10-28 15:42:14 +0600

    switch case practice

---
+++ HomePractice11/HomePractice11/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace HomePractice11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region switch case practice
+            var choice = Convert.ToInt32(Console.ReadLine());
+            int a = 5;
+            int b = 6;
+            double c;
+            switch(choice)
+            {
+                case 1:
+                    c = a + b;
+                    Console.WriteLine($"The addition is = {c}");
+                    break;
+
+                case 2:
+                    c = a - b;
+                    Console.WriteLine($"The sub is = {c}");
+                    break;
+
+                case 3:
+                    c = a * b;
+                    Console.WriteLine($"The mul is = {c}");
+                    break;
+
+                case 4:
+                    c = a / b;
+                    Console.WriteLine($"The div is = {c}");
+                    break;
+                default:
+                    Console.WriteLine("Ki dili ?");
+                    break; 
+            }
+            #endregion
+        }
+    }
+}
