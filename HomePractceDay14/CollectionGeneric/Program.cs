9acf0426da8ff3509af425546644e7f0b7ba1feb
Abdullah Basar Maruf <marufcs063@gmail.com>
2020-11-18 02:52:04 +0600

    CollectionGeneric practice

---
+++ HomePractceDay14/CollectionGeneric/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Dictonary 
+            Dictionary<string, string> openWith = new Dictionary<string, string>();
+            openWith.Add("txt", "notepad.exe");
+            openWith.Add("bmp", "paint.exe");
+            openWith.Add("dib", "paint.exe");
+            openWith.Add("rtf", "wordpad.exe");
+
+            try
+            {
+                openWith.Add("txt", "winword.exe");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("An element with Key = \"txt\" already exists.");
+            }
+
+            Console.WriteLine("For key = \"rtf\", value = {0}.", openWith["rtf"]);
+            
+            openWith["rtf"] = "winword.exe";
+            Console.WriteLine("For key = \"rtf\", value = {0}.",
+                openWith["rtf"]);
+
+            openWith["doc"] = "winword.exe";
+
+            string value = "";
+            if (openWith.TryGetValue("doc", out value))
+            {
+                Console.WriteLine("For key = \"doc\", value = {0}.", value);
+            }
+            else
+            {
+                Console.WriteLine("Key = \"doc\" is not found.");
+            }
+
+            openWith.ContainsKey("abc");
+            openWith.ContainsValue("window.xp");
+            openWith.Remove("doc");
+            openWith.TryAdd("docx", "alhamdulillah");
+
+            Console.WriteLine();
+            foreach (KeyValuePair<string, string> kvp in openWith)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}",
+                    kvp.Key, kvp.Value);
+            }
+            #endregion
+
+            #region 
+
+            #endregion
+        }
+    }
+}
---
+++ HomePractceDay14/HomePractceDay14/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomePractceDay14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region List
+            List<string> dinosaurs = new List<string>();
+            Console.WriteLine("\nCapacity: {0}",dinosaurs.Capacity);
+            dinosaurs.Add("Tyrannosaurus");
+            dinosaurs.Add("Amargasaurus");
+            dinosaurs.Add("Mamenchisaurus");
+            dinosaurs.Add("Deinonychus");
+            dinosaurs.Add("Compsognathus");
+           
+            Console.WriteLine();
+            foreach (string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
+
+            Console.WriteLine($"Capacity: {dinosaurs.Capacity} ");
+            Console.WriteLine($"Count: {dinosaurs.Count}");
+            Console.WriteLine($"Contain (\"Deinonychus\") {dinosaurs.Contains("Deinonychus")}");
+            
+            dinosaurs.Insert(3, "BD Dinosaurs");
+            dinosaurs.Insert(4, "Afg Dinosaurs");
+            dinosaurs.Insert(5, "Pak Dinosaurs");
+            Console.WriteLine();
+
+            foreach(string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
+            
+            Console.WriteLine("\ndinosaurs[3]: {0}",dinosaurs[3]);
+            Console.WriteLine("\nRemove (\"Compsognathus\")");
+
+            dinosaurs.Remove("Compsognathus");
+            Console.WriteLine();
+
+            foreach(string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
+            dinosaurs.TrimExcess();
+            Console.WriteLine("\nTrimExcess()");
+
+            Console.WriteLine("\n Capacity: {0}",dinosaurs.Capacity);
+            Console.WriteLine("\nCount: {0}",dinosaurs.Count);
+
+            dinosaurs.Sort();
+            Console.WriteLine(dinosaurs.BinarySearch("Deinonychus"));
+            foreach (string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
+
+            Console.WriteLine("\nBinarySearch and Insert \"Coelophysis\":");
+            int index = dinosaurs.BinarySearch("Coelophysis");
+            if (index < 0)
+            {
+                dinosaurs.Insert(~index, "Coelophysis");
+            }
+
+            Console.WriteLine();
+            foreach (string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
+
+            Console.WriteLine();
+           
+
+            Console.WriteLine("\nAddRange(dinosaurs)");
+            dinosaurs.AddRange(dinosaurs);
+
+            Console.WriteLine();
+            foreach (string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
+            
+            Console.WriteLine("\nRemoveRange(2, 2)");
+            dinosaurs.RemoveRange(0, 8);
+
+            Console.WriteLine();
+            foreach (string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
+
+            dinosaurs.Reverse();
+
+            Console.WriteLine();
+            foreach (string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
+
+            dinosaurs.Reverse(1, 4);
+
+            Console.WriteLine();
+            foreach (string dinosaur in dinosaurs)
+            {
+                Console.WriteLine(dinosaur);
+            }
+
+
+            dinosaurs.Clear();
+            Console.WriteLine("\nClear");
+            Console.WriteLine("Capacity: {0}",dinosaurs.Capacity);
+            Console.WriteLine("Count: {0}",dinosaurs.Count);
+
+            #endregion
+
+            #region sortedlist generics
+
+
+            #endregion
+
+
+
+
+
+
+        }
+    }
+}
