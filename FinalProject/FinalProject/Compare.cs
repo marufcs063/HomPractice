75f57105aaafbc07552f67dc497b0cab5502f967
Abdullah Basar Maruf <marufcs063@gmail.com>
2020-12-15 21:38:03 +0600

    Done!

---
+++ FinalProject/FinalProject/Compare.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProject
+{
+    class Compare
+    {
+        #region comp1
+        public Boolean isSame1(Stack<String> tower1,
+                            Stack<String> comp1)
+        {
+           
+            Boolean flag = true;
+
+            
+            if (tower1.Count != comp1.Count)
+            {
+                flag = false;
+                return flag;
+            }
+
+            
+            while (tower1.Count != 0)
+            {
+                
+                if (tower1.Peek() == comp1.Peek())
+                {
+                      
+                    tower1.Pop();
+                    comp1.Pop();
+                }
+                else
+                {
+                    
+                    flag = false;
+                    break;
+                }
+            }
+
+             
+            return flag;
+        }
+        #endregion
+        #region comp2
+        public Boolean isSame2(Stack<String> tower2,
+                           Stack<String> comp2)
+        {
+
+            Boolean flag = true;
+
+
+            if (tower2.Count != comp2.Count)
+            {
+                flag = false;
+                return flag;
+            }
+
+
+            while (tower2.Count != 0)
+            {
+
+                if (tower2.Peek() == comp2.Peek())
+                {
+
+                    tower2.Pop();
+                    comp2.Pop();
+                }
+                else
+                {
+
+                    flag = false;
+                    break;
+                }
+            }
+
+
+            return flag;
+        }
+        #endregion
+        #region comp3
+        public Boolean isSame3(Stack<String> tower3,
+                           Stack<String> comp3)
+        {
+
+            Boolean flag = true;
+
+
+            if (tower3.Count != comp3.Count)
+            {
+                flag = false;
+                return flag;
+            }
+
+
+            while (tower3.Count != 0)
+            {
+
+                if (tower3.Peek() == comp3.Peek())
+                {
+
+                    tower3.Pop();
+                    comp3.Pop();
+                }
+                else
+                {
+
+                    flag = false;
+                    break;
+                }
+            }
+
+
+            return flag;
+        }
+        #endregion
+    }
+}
--- FinalProject/FinalProject/Program.cs
+++ FinalProject/FinalProject/Program.cs
@@ -10,7 +10,7 @@ namespace FinalProject
             #region randomization part
             string[] colors = { "R", "G", "B" };
             colors.Randomizer();
-                            
+                                            
             Stack<string> tower1 = new Stack<string>();
             tower1.Push(colors[0]);
             tower1.Push(colors[1]);
@@ -25,27 +25,78 @@ namespace FinalProject
             tower3.Push(colors[1]);
             tower3.Push(colors[0]);
             tower3.Push(colors[2]);
-
+            Stack<string> comp1 = new Stack<string>();
+            comp1.Push("R");
+            comp1.Push("R");
+            comp1.Push("R");
+            Stack<string> comp2 = new Stack<string>();
+            comp2.Push("G");
+            comp2.Push("G");
+            comp2.Push("G");
+            Stack<string> comp3 = new Stack<string>();
+            comp3.Push("B");
+            comp3.Push("B");
+            comp3.Push("B");
+            Console.WriteLine("Given towers>>>: \n");
+            Console.Write("1 top =>   ");
             foreach (string s in tower1)
             {
-                Console.Write($"{s} ");                       
+                Console.Write($"{s} ");
             }
+            Console.Write("||");
+
             Console.WriteLine();
+            Console.Write("2 top =>   ");
             foreach (string s in tower2)
             {
                 Console.Write($"{s} ");
             }
+            Console.Write("||");
             Console.WriteLine();
+            Console.Write("3 top =>   ");
             foreach (string s in tower3)
             {
                 Console.Write($"{s} ");
             }
+            Console.Write("||");
+
+            Console.WriteLine("\n\nYou Have to make like this!");
+            Console.Write("1 top =>   ");
+            foreach (string s in comp1)
+            {
+                Console.Write($"{s} ");
+            }
+            Console.Write("||");
+
+            Console.WriteLine();
+            Console.Write("2 top =>   ");
+            foreach (string s in comp2)
+            {
+                Console.Write($"{s} ");
+            }
+            Console.Write("||");
+            Console.WriteLine();
+            Console.Write("3 top =>   ");
+            foreach (string s in comp3)
+            {
+                Console.Write($"{s} ");
+            }
+            Console.Write("||");
+
+
+            Console.WriteLine("\n\nPlease enter source and destination value as like '1  2': \n Note: please Match 1 then 2 then 3... ");
             #endregion
+            
+
 
             #region from to
+            var Try = 0;
+            start:
+            Console.WriteLine("\n");
             var input = Console.ReadLine().Split(' ');
             var from = int.Parse(input[0]);
             var to = int.Parse(input[1]);
+            
             string[] hold =new string[1];
             if (from==1)
             {
@@ -67,19 +118,19 @@ namespace FinalProject
             {
                 Console.WriteLine("Pleae enter the source to destination value");
             }
-            string[] keep = new string[1];
+            
             if (to == 1)
             {
-                keep[0] = tower1.Push(hold[0]);
+                tower1.Push(hold[0]);
                 
             }
             else if (to== 2)
             {
-                keep[0] = tower2.Push(hold[0]);
+                tower2.Push(hold[0]);
             }
             else if (to == 3)
             {
-                keep[0] = tower3.Push(hold[0]);
+                tower3.Push(hold[0]);
             }
                 
 
@@ -88,7 +139,56 @@ namespace FinalProject
                 Console.WriteLine("Pleae enter the source to destination value");
             }
 
+
+
+            Console.Write("1 top =>   ");
+            foreach (string s in tower1)
+            {
+                Console.Write($"{s} ");
+            }
+            Console.Write("||");
+
+            Console.WriteLine();
+            Console.Write("2 top =>   ");
+            foreach (string s in tower2)
+            {
+                Console.Write($"{s} ");
+            }
+            Console.Write("||");
+            Console.WriteLine();
+            Console.Write("3 top =>   ");
+            foreach (string s in tower3)
+            {
+                Console.Write($"{s} ");
+            }
+            Console.Write("||");
             #endregion
+
+            #region compare 
+
+            Compare compare = new Compare();
+            if (compare.isSame3(tower3, comp3))
+            {
+                if( compare.isSame2(tower2, comp2) )
+                {
+                    if ( compare.isSame1(tower1, comp1))
+                    {
+                        Console.WriteLine("\nCongratulations! You have matched all the towers!.\n");
+                    }
+
+                }
+
+            }
+            else
+            {
+                Try+= 1;
+                Console.WriteLine($"\n\nYou have tried for {Try} times.\nPlease Try again!..." );
+                goto start;
+            }
+            #endregion
+
+            Console.WriteLine($"\n\nYour attempt number is: {Try} ");
+
         }
     }
 }
