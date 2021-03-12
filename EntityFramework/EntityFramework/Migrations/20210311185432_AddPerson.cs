c81d00e2bdd99da8c5216f78cfa15bfc59a15f13
Abdullah Basar Maruf <marufcs063@gmail.com>
2021-03-12 19:55:50 +0600

    last updated

---
+++ EntityFramework/EntityFramework/Migrations/20210311185432_AddPerson.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFramework.Migrations
+{
+    public partial class AddPerson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Perosns",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: false),
+                    Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Perosns", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Perosns");
+        }
+    }
+}
--- EntityFramework/EntityFramework/Program.cs
+++ EntityFramework/EntityFramework/Program.cs
@@ -6,22 +6,22 @@ namespace EntityFramework
     {
         static void Main(string[] args)
         {
-            using (PersonContext pc = new PersonContext())
-            {
-                var person = new Person() { Name = "Isaac Newton" };
-                pc.Add(person);
-                //pc.Perosns.AddRange
-                //    (
-                //     new Person { Name = "Isaac Newton" },
-                //     new Person { Name = "C.F Gauss" },
-                //     new Person { Name = "Albert Einstein" }
-                //     );
+            //using (PersonContext pc = new PersonContext())
+            //{
+            //    var person = new Person() { Name = "Isaac Newton" };
+            //    pc.Add(person);
+            //    //pc.Perosns.AddRange
+            //    //    (
+            //    //     new Person { Name = "Isaac Newton" },
+            //    //     new Person { Name = "C.F Gauss" },
+            //    //     new Person { Name = "Albert Einstein" }
+            //    //     );
 
 
 
 
-                pc.SaveChanges();
-            }
+            //    pc.SaveChanges();
+            //}
         }
     }
 }
